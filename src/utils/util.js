4bf332fa9ae3ef6f7ba4a13de56827a4f2e26ae0
jyg <opistla1@naver.com>
2021-06-30 19:03:26 +0900

    test

--- src/utils/util.js
+++ src/utils/util.js
@@ -2,6 +2,7 @@ export const util = {
 
   // 3자리 콤마
   numberCommas(num) {
+    console.log('test');
     return typeof num === 'number' ?num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") : num;
   }
 };
